dcd949bb9b309f086c4e29857905a6574f344105
Juan carlos chocllu <j.chocllu@opti-system.com>
2026-03-20 14:47:54 -0400

    cmbios

--- src/module/venta/interface/venta.ts
+++ src/module/venta/interface/venta.ts
@@ -1,3 +1,4 @@
+import type { ImagenesI } from "../../producto/interface/producto"
 
 export interface carritoI {
     codigo: string
@@ -23,3 +24,31 @@ export interface listarVentaI {
     fechaPedido: string
 }
 
+
+export interface DetalleVentaI {
+  detalle: Detalle[];
+  venta: Venta;
+}
+
+export interface Detalle {
+  _id: string;
+  cantidad: number;
+  descripcion: string;
+  imagen: ImagenesI[];
+  precioTotal: number;
+  precioUnitario: number;
+}
+
+
+
+export interface Venta {
+  _id: string;
+  apellidos: string;
+  codigo: string;
+  fechaFinalizado: string; // o Date
+  fechaPedido: string;     // o Date
+  nombre: string;
+  total: number;
+  totalConDescuento: number;
+  tracking: string;
+}
